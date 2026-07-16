ddc54893e45de78a12b4a1308a0128795b757012
PRADEEP PATEL <pradeep4ies@gmail.com>
2026-07-16 17:56:41 +0530

    feat: implement user login and current user retrieval functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,4 +63,80 @@ export async function loginUser(req,res){
             message:"Both fields are required."
         })
     }
+
+    try{
+        const user=await User.findOne({email});
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:"Invalid email or password"
+            })
+        }
+
+        const match=await bcrypt.compare(password, user.password);
+        if(!match){
+            return res.status(401).json({
+                success:false,
+                message:"Invalid email or password"
+            })
+        }
+
+        const token=createToken(user._id);
+        res.json({
+            success:true,
+            token,
+            user:{
+                id:user._id,
+                name:user.name,
+                email:user.email
+            }
+        })
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({
+            success:false,
+            message:"Server Error."
+        })
+    }
+}
+
+
+//to get the login user details
+export async function getCurrentUser(req,res){
+    try{
+       const user=await User.findById(req.user.id).select("name email");
+      if(!user){
+        return res.status(404).json({
+            success:false,
+            message:"User not found."
+        })
+      }
+      res.json({success:true,user});  
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({
+            success:false,
+            message:"server error."
+        })
+    }
+}
+
+
+//to update a user profile
+export async function updateProfile(req,res){
+    const{name, email}=req.body;
+    if(!name || !email || !validator.isEmail(email)){
+        return res.status(400).json({
+            success:false,
+            message:"Valid email and name are required."
+        })
+    }
+    try{
+
+    }
+    catch(err){
+        
+    }
 }
