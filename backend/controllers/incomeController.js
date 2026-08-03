01db254fc2604197bc48920f123d08f1459f4ba2
PRADEEP PATEL <pradeep4ies@gmail.com>
2026-08-03 23:08:44 +0530

    feat: implement addIncome function in income controller

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,14 @@
+import incomeModel from '../models/incomeModel.js';
+
+//add income
+export async function addIncome(req,res){
+    const userId=req.user._id;
+    const {description, amount, category, date}=req.body;
+
+    try{
+
+    }
+    catch(err){
+
+    }
+}
