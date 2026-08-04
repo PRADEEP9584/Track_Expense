140504b5a7131235ac2bbb33551f8c58a912268a
PRADEEP PATEL <pradeep4ies@gmail.com>
2026-08-04 23:39:02 +0530

    feat: enhance addIncome function with input validation and error handling

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -6,9 +6,34 @@ export async function addIncome(req,res){
     const {description, amount, category, date}=req.body;
 
     try{
+        if(!description || !amount || !category || !date){
+            return res.status(400).json({
+                success:false,
+                message:"All fields are required"
+            });
+        }
 
+        const newIncome=new incomeModel({
+            userId,
+            description,
+            amount,
+            category,
+            date:new Date(date)
+        });
+        await newIncome.save()
+        res.json({
+            success:true,
+            message:"Income added successfully"
+        });
     }
-    catch(err){
-
+    catch(error){
+console.log(error);
+res.status(500).json({
+    success:false,
+    message:"Server Error"
+})
     }
 }
+
+
+//to get all income(all)
