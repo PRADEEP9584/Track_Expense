03ba4fae05c2490a52664875dda432dea798345d
PRADEEP PATEL <pradeep4ies@gmail.com>
2026-08-15 23:15:28 +0530

    feat: implement addExpense function to handle expense creation

---
+++ backend/controllers/expenseController.js
@@ -0,0 +1,38 @@
+import expenseaModel from '../models/expenseModel.js';
+import getDateRange from '../utils/getDateRange.js';
+
+//add expense
+export async function addExpense(req, res){
+    const userId=req.user._id;
+    const {description, amount, category, date}=req.body;
+
+    try{
+        if(!description || !amount || !category || !date){
+            return res.status(400).json({
+                success:false,
+                message:"All fields are required"
+            });
+        }
+        const newExpense=new expenseModel({
+            userId,
+            description,
+            amount,
+            category,
+            date:new Date(date)
+        });
+        await newExpense.save()
+        res.json({
+            success:true,
+            message:"Expense added successfully"
+        });
+    }    
+        catch(error){
+console.log(error);
+res.status(500).json({
+    success:false,
+    message:"Server Error"
+})
+    }
+}
+
+//to get all expense
